aca7f15d88272b16f335280ee8327bfd3560e701
Mikolenn <castroariasricardo@gmail.com>
2020-11-10 11:29:21 -0600

    View funciona bien

--- taller/urls.py
+++ taller/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('', views.home, name='base'),
     path('', include("django.contrib.auth.urls")),
     path('view/', views.view, name='view'),
+    path('view/<int:pk>', views.view, name='view'),
     path('create/', views.create, name='create'),
 ]

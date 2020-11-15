abdb035626f596c613815aa4f37edeb456e3f608
Mikolenn <castroariasricardo@gmail.com>
2020-11-14 18:14:02 -0600

    nada

--- register/views.py
+++ register/views.py
@@ -7,16 +7,6 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 # Create your views here.
 
-# def register(response):
-#     if response.method == "POST":
-#         form=RegisterForm(response.POST)
-#         if form.is_valid():
-#             form.save()
-#         return redirect("/")
-#     else:
-#         form=RegisterForm()
-#
-#     return render(response, "register/register.html", {"form":form})
 
 def register (request):
 
